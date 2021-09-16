1a37f5920bd972a3125c6efee8fc622d95564dd6
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-16 19:03:00 +0530

    Solution File

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("********Welcome to Snake and Ladder Game********");
+
+            UC1_SL.SL();
+            UC2_SL.Dice();
+            UC3_SL.Option();
+            UC4_SL.restart();
+            UC5_SL.win();
+            UC6_SL.DieCount();
+            UC7_SL.TwoPlayer();
+        }
+    }
+}
