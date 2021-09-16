331cfd84b95db46bbd3280c27f59e9fbc4944e28
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-16 18:58:13 +0530

    die rolled

---
+++ UC2_SL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeAndLadder
+{
+    class UC2_SL
+    {
+        public static void Dice()
+        {
+            Random die = new Random();
+            int Dice = die.Next(1, 7);
+            Console.WriteLine("Dice obtain is "+Dice);
+        }
+    }
+}
