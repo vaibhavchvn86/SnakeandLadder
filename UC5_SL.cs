c96d2e2623c7eba6c12007758a8ca0a835c01261
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-16 19:01:04 +0530

    exact win position

---
+++ UC5_SL.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeAndLadder
+{
+    class UC5_SL
+    {
+        public static void win()
+        {
+            int position = 0;
+            int diecount = 0;
+            while (position != 100)
+            {
+                Random die = new Random();
+                int a = die.Next(1, 7);
+                Console.WriteLine("Dice obtain is " + a);
+                Random option = new Random();
+                int Opt = option.Next(1, 4);
+                diecount++;
+                switch (Opt)
+                {
+                    case 1:
+                        Console.WriteLine("Player chosse No Play");
+                        break;
+                    case 2:
+                        Console.WriteLine("Player got a Snake position will be decrease");
+                        if ((position - a) < 0)
+                            position = 0;
+                        else
+                            position -= a;
+                        break;
+                    case 3:
+                        Console.WriteLine("Player got a Ladder");
+                        if (position > 100)
+                            position = position;
+                        else
+                            position += a;
+                        Console.WriteLine("Player Won the game");
+                        break;
+                }
+                Console.WriteLine("player position " + position);
+            }
+        }
+    }
+}
