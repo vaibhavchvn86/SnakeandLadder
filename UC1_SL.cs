26e1d9091b9e0968d3b477fec3d1ed1730d20d44
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-16 18:48:56 +0530

    Start Position

---
+++ UC1_SL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeAndLadder
+{
+    class UC1_SL
+    {
+        public static void SL()
+        {
+            int Playerpositon = 0;
+            Console.WriteLine("Player is at Start Position " + Playerpositon);
+        }
+    }
+}
